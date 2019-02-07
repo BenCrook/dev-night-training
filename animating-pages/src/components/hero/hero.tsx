f262f6b54de6c9b082e80b21a32c00f381b4aeb8
Ben Crook <ben@space48.com>
2019-02-07 23:34:16 +0000

    * Update to react and react-dom to 16.8.0 * Use React hooks * Only preload the next image and only once current image has finished loading * Various refactoring * Add various todos

--- animating-pages/src/components/hero/hero.tsx
+++ animating-pages/src/components/hero/hero.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import anime from 'animejs';
 import styles from './hero.module.css';
 import initialBannerImage from './images/alireza-khajehali-1.jpg';
@@ -6,22 +6,14 @@ import secondBannerImage from './images/sunset-mountain.jpg';
 import thirdBannerImage from './images/snow-mountain-2.jpg';
 import fourthBannerImage from './images/snow-mountain-e.jpg';
 
-interface PropsInterface {
-
-}
-
-interface StateInterface {
-    activeBanner: number
-}
-
 interface HeroInterface {
     image: string,
     text: string
 }
 
-// We could also use HeroInterface[] here but I find Array<HeroInterface> to be more explicit and consistent.
+// We could also use Array<HeroInterface> here but I find HeroInterface[] to be more concise.
 // See https://toddmotto.com/typing-arrays-typescript for more methods
-const heroData: Array<HeroInterface> = [
+const heroData: HeroInterface[] = [
     {
         image: initialBannerImage,
         text: 'Text Goes Here - First Slide'
@@ -40,100 +32,111 @@ const heroData: Array<HeroInterface> = [
     }
 ];
 
-class Hero extends Component<PropsInterface, StateInterface> {
-    constructor(props: PropsInterface) {
-        super(props);
+/**
+ * Preloads specified banner image so there isn't a flash or noticeable load during slides
+ * todo: Only preload the second banner image after the first has loaded.
+ * todo: Idea for this - Function that loads the image then sets it
+ * todo: Split preload functions into their own file
+ * todo: Throw error?
+ */
+const preloadBannerImage = (bannerIndexToPreload: number) => {
+    // Check image exists else it will error
+    if (heroData[bannerIndexToPreload]) {
+        console.log(`Preloading ${heroData[bannerIndexToPreload].image}`);
+        const img = new Image();
+        img.src = heroData[bannerIndexToPreload].image;
+    }
+};
 
-        this.state = {
-            activeBanner: 0
+/**
+ * Fades in the banner using anime.js
+ */
+const animateBanner = () => {
+    anime({
+        duration: 600,
+        easing: 'easeInOutQuad',
+        targets: '.animation',
+        rotate: [-0.5, 0],
+        scale: [1.05, 1],
+        skewX: [0.5, 0]
+    })
+};
+
+/**
+ * Returns true if the banner can be updated, false if it cannot. Supports an optional callback function.
+ * @param {string} direction - 'next' or 'previous' - The slide direction.
+ * @param {number} activeBanner - The index of the current banner.
+ * @param {function} [callback] - An optional callback function, currently updates the active banner.
+ */
+const canBannerUpdate = (direction: string, activeBanner: number, callback?: (newBannerIndex: number) => void) => {
+    const heroLength: number = heroData.length;
+    const canDecrement: boolean = activeBanner > 0;
+    const canIncrement: boolean = activeBanner < heroLength - 1; // -1 as arrays are 0 index based
+
+    // todo: Prevent repeated code here
+    if (direction === 'next' && canIncrement) {
+        if (callback) {
+            callback(activeBanner + 1);
         }
-    }
-
-    /**
-     * Methods to run once the component has mounted
-     */
-    componentDidMount() {
-        Hero.fadeInBanner();
-        this.preLoadBannerImages();
-    }
-
-    /**
-     * Preloads images so three isn't a white flash during slides
-     */
-    preLoadBannerImages() {
-        heroData.forEach((banner) => {
-            const img = new Image();
-            img.src = banner.image;
-        })
-    }
-
-    /**
-     * Fades in the banner.
-     * This is a static method as it remains the same across all instances of this class
-     */
-    static fadeInBanner() {
-        anime({
-            duration: 600,
-            easing: 'easeInOutQuad',
-            targets: '.animation',
-            rotate: [-0.5, 0],
-            scale: [1.05, 1],
-            skewX: [0.5, 0]
-        })
-    }
-
-    /**
-     * Updates the hero
-     * @param {string} direction - 'next' or 'previous' - The slide direction.
-     */
-    updateHero(direction: string) {
-        const activeBanner: number = this.state.activeBanner;
-        const heroLength: number = heroData.length;
-        const canDecrement: boolean = activeBanner > 0;
-        const canIncrement: boolean = activeBanner < heroLength - 1; // -1 as length is 0 index based
-
-        if (direction === 'next' && canIncrement) {
-            this.setState((state) => {
-                return {
-                    activeBanner: state.activeBanner + 1
-                };
-            });
-        } else if (direction === 'previous' && canDecrement) {
-            this.setState((state) => {
-                return {
-                    activeBanner: state.activeBanner - 1
-                };
-            });
-        } else {
-            return
+        return true;
+    } else if (direction === 'previous' && canDecrement) {
+        if (callback) {
+            callback(activeBanner - 1);
         }
-
-        Hero.fadeInBanner();
+        return true;
     }
 
-    render() {
-        const state: StateInterface = this.state;
-        const activeBanner: number = state.activeBanner;
+    return false;
+};
 
-        //@toDo: Tidy up animations, may need to render all slides out
-        return (
-            <div className={`${styles.container}`}>
-                <div className={`${styles.image} animation`} style={{backgroundImage: `url(${heroData[activeBanner].image})`}} />
-                <div className={styles.content}>
-                    <div className={styles.text}>
-                        {heroData[activeBanner].text}
-                    </div>
-                    <div className={styles.links}>
-                        <h2>Links Go Here</h2>
-                    </div>
-                    <div className={styles.arrows}>
-                        <h2 onClick={() => this.updateHero('next')} className={`${styles.arrow} ${styles.next}`}>NEXT</h2>
-                        <h2 onClick={() => this.updateHero('previous')} className={`${styles.arrow} ${styles.next}`}>PREV</h2>
-                    </div>
+const setBannerImage = (imageSrc: string, activeBanner: number) => {
+    console.log(`Loading ${imageSrc}`);
+    const img = new Image();
+    img.src = imageSrc;
+
+    img.onload = function () {
+        console.log(`${imageSrc} HAS LOADED!`);
+        preloadBannerImage(activeBanner + 1);
+    };
+
+    return imageSrc;
+};
+
+const Hero = () => {
+    const [activeBanner, setActiveBanner] = useState(0);
+    const valuesToTriggerRender: any[] = [activeBanner];
+
+    // Re-render only happens if the values inside valuesToTriggerRender change
+    useEffect(() => {
+        animateBanner();
+    }, valuesToTriggerRender);
+
+    // todo: Use React.Context or Redux rather than passing activeBanner through to multiple components
+    // todo: Improve animations, may need to render all slides out for this
+    // todo: Preload the next image on hover rather than automatically?
+    //     Currently we presume the user will change slide, will not always be the case
+    //     Will be less impactful but will still load the image slightly earlier than without
+    return (
+        <div className={`${styles.container}`}>
+            <div className={`${styles.image} animation`}
+                 style={{backgroundImage: `url(${setBannerImage(heroData[activeBanner].image, activeBanner)})`}}/>
+            <div className={styles.content}>
+                <div className={styles.text}>
+                    {heroData[activeBanner].text}
+                </div>
+                <div className={styles.links}>
+                    <h2>Links Go Here</h2>
+                </div>
+                <div className={styles.arrows}>
+                    <h2 onClick={() => canBannerUpdate('next', activeBanner, setActiveBanner)}
+                        className={`${styles.arrow} ${styles.next}`}>NEXT</h2>
+                    <h2 onClick={() => canBannerUpdate('previous', activeBanner, setActiveBanner)}
+                        className={`${styles.arrow} ${styles.next}`}>PREV</h2>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
+
 
 export default Hero;

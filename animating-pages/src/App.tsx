ca589efb731bc5fdc9ecd1877416765f235616d8
Ben Crook <info@bencrook.co.uk>
2019-02-08 22:28:42 +0000

    Rename hero directory to banner

--- animating-pages/src/App.tsx
+++ animating-pages/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Header from './components/header/header'
-import Banner from './components/hero/banner';
+import Banner from './components/banner/banner';
 
 class App extends Component {
     render() {
